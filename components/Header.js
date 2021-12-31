60f79b95de652b453cab19c6921cf43f1026ce4b
tangly1024 <tlyong1992@hotmail.com>
2021-12-31 17:46:31 +0800

    feature: header加入头像

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,8 @@
 import BLOG from '@/blog.config'
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useEffect, useState } from 'react'
 import Typed from 'typed.js'
-
-const wrapperTop = 0
+import Image from 'next/image'
+import Link from 'next/link'
 
 /**
  *
@@ -30,23 +28,26 @@ export default function Header () {
   return (
     <header
       id="header"
-      className="duration-500 md:bg-fixed bg-cover bg-center mb-12 h-96 mx-5 md:mx-0 md:rounded-3xl bg-black"
+      className="duration-500 md:bg-fixed bg-cover bg-center mb-24 h-96 mx-5 md:mx-0 md:rounded-3xl bg-black"
       style={{
-        backgroundImage:
-          `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0,0,0,0.2), rgba(0, 0, 0, 0.8) ),url("${BLOG.bannerImage}")`
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0,0,0,0.2), rgba(0, 0, 0, 0.8) ),url("${BLOG.bannerImage}")`
       }}
     >
       <div className=" flex h-96 items-center justify-center w-full text-4xl md:text-7xl text-white">
-        <div id='typed' className='flex text-center font-serif'/>
+        <div id="typed" className="flex text-center font-serif" />
       </div>
-      <div
-        onClick={() => {
-          window.scrollTo({ top: wrapperTop, behavior: 'smooth' })
-        }}
-        className="cursor-pointer w-full text-center py-4 text-5xl absolute bottom-10 text-white"
-      >
-        <FontAwesomeIcon icon={faAngleDown} className='animate-bounce'/>
-      </div>
+      <Link passHref href={'/about'}>
+        <div className="hover:rotate-45 hover:scale-125 transform duration-200 -mt-20 cursor-pointer flex w-full justify-center">
+            <Image
+              alt={BLOG.title}
+              width={120}
+              height={120}
+              loading="lazy"
+              src="/avatar.jpg"
+              className="rounded-full border-black"
+            />
+          </div>
+      </Link>
     </header>
   )
 }
